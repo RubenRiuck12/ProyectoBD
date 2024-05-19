60e6bb625b0649fda38dc7bf83ccc28cda234b0f
Alejandro <L21021165@veracruz.tecnm.mx>
2024-05-19 17:33:51 -0600

    Interfaz de Backup-Restore

--- ProyectoABD/src/Vista/Voluntarios.java
+++ ProyectoABD/src/Vista/Voluntarios.java
@@ -10,12 +10,12 @@ import java.awt.BorderLayout;
  *
  * @author aleja
  */
-public class Voluntarios2 extends javax.swing.JFrame {
+public class Voluntarios extends javax.swing.JFrame {
 
     /**
      * Creates new form Voluntarios2
      */
-    public Voluntarios2() {
+    public Voluntarios() {
         initComponents();
     }
 
@@ -234,20 +234,21 @@ public class Voluntarios2 extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Voluntarios2.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Voluntarios.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Voluntarios2.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Voluntarios.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Voluntarios2.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Voluntarios.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Voluntarios2.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Voluntarios.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new Voluntarios2().setVisible(true);
+                new Voluntarios().setVisible(true);
             }
         });
     }
