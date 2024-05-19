b8f571c5413f02fe67bea881c65eca35e4b5f441
Alejandro <L21021165@veracruz.tecnm.mx>
2024-05-19 14:11:35 -0600

    Interfaz de Voluntarios2 ahora si

--- ProyectoABD/src/Vista/Voluntarios2.java
+++ ProyectoABD/src/Vista/Voluntarios2.java
@@ -10,12 +10,13 @@ import java.awt.BorderLayout;
  *
  * @author aleja
  */
-public class Voluntarios extends javax.swing.JFrame {       
-       
-    
-       
-    public Voluntarios() {
-        initComponents();        
+public class Voluntarios2 extends javax.swing.JFrame {
+
+    /**
+     * Creates new form Voluntarios2
+     */
+    public Voluntarios2() {
+        initComponents();
     }
 
     /**
@@ -31,8 +32,8 @@ public class Voluntarios extends javax.swing.JFrame {
         jMenuBar1 = new javax.swing.JMenuBar();
         jMenu1 = new javax.swing.JMenu();
         jMenuItem1 = new javax.swing.JMenuItem();
-        jMenuItem3 = new javax.swing.JMenuItem();
         jMenuItem2 = new javax.swing.JMenuItem();
+        jMenuItem3 = new javax.swing.JMenuItem();
         jMenu2 = new javax.swing.JMenu();
         jMenuItem4 = new javax.swing.JMenuItem();
         jMenuItem5 = new javax.swing.JMenuItem();
@@ -41,7 +42,6 @@ public class Voluntarios extends javax.swing.JFrame {
         jMenuItem7 = new javax.swing.JMenuItem();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setBackground(new java.awt.Color(0, 51, 102));
 
         content.setBackground(new java.awt.Color(204, 204, 204));
 
@@ -53,19 +53,10 @@ public class Voluntarios extends javax.swing.JFrame {
         );
         contentLayout.setVerticalGroup(
             contentLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 384, Short.MAX_VALUE)
+            .addGap(0, 361, Short.MAX_VALUE)
         );
 
-        jMenuBar1.setBackground(new java.awt.Color(0, 51, 102));
-        jMenuBar1.setToolTipText("");
-
-        jMenu1.setBackground(new java.awt.Color(0, 51, 102));
         jMenu1.setText("Administrar Voluntarios");
-        jMenu1.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jMenu1ActionPerformed(evt);
-            }
-        });
 
         jMenuItem1.setText("Registrar");
         jMenuItem1.addActionListener(new java.awt.event.ActionListener() {
@@ -75,21 +66,21 @@ public class Voluntarios extends javax.swing.JFrame {
         });
         jMenu1.add(jMenuItem1);
 
-        jMenuItem3.setText("Lista");
-        jMenuItem3.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jMenuItem3ActionPerformed(evt);
-            }
-        });
-        jMenu1.add(jMenuItem3);
-
-        jMenuItem2.setText("Actualizar");
+        jMenuItem2.setText("Lista");
         jMenuItem2.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jMenuItem2ActionPerformed(evt);
             }
         });
         jMenu1.add(jMenuItem2);
+
+        jMenuItem3.setText("Actualizar");
+        jMenuItem3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem3ActionPerformed(evt);
+            }
+        });
+        jMenu1.add(jMenuItem3);
 
         jMenuBar1.add(jMenu1);
 
@@ -149,6 +140,17 @@ public class Voluntarios extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void jMenuItem7ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem7ActionPerformed
+        ActualizarProfesion ap= new ActualizarProfesion();
+        ap.setSize(625,384);
+        ap.setLocation(0,0);
+        
+        content.removeAll();
+        content.add(ap, BorderLayout.CENTER);
+        content.revalidate();
+        content.repaint();
+    }//GEN-LAST:event_jMenuItem7ActionPerformed
+
     private void jMenuItem1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem1ActionPerformed
         RegistrarVoluntarios rv= new RegistrarVoluntarios();
         rv.setSize(625,384);
@@ -161,21 +163,6 @@ public class Voluntarios extends javax.swing.JFrame {
     }//GEN-LAST:event_jMenuItem1ActionPerformed
 
     private void jMenuItem2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem2ActionPerformed
-        ActualizarVoluntarios av= new ActualizarVoluntarios();
-        av.setSize(625,384);
-        av.setLocation(0,0);
-        
-        content.removeAll();
-        content.add(av, BorderLayout.CENTER);
-        content.revalidate();
-        content.repaint();
-    }//GEN-LAST:event_jMenuItem2ActionPerformed
-
-    private void jMenuItem3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem3ActionPerformed
-
-    }//GEN-LAST:event_jMenuItem3ActionPerformed
-
-    private void jMenu1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenu1ActionPerformed
         ListaVoluntarios lv= new ListaVoluntarios();
         lv.setSize(625,384);
         lv.setLocation(0,0);
@@ -184,7 +171,18 @@ public class Voluntarios extends javax.swing.JFrame {
         content.add(lv, BorderLayout.CENTER);
         content.revalidate();
         content.repaint();
-    }//GEN-LAST:event_jMenu1ActionPerformed
+    }//GEN-LAST:event_jMenuItem2ActionPerformed
+
+    private void jMenuItem3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem3ActionPerformed
+        ActualizarVoluntarios av= new ActualizarVoluntarios();
+        av.setSize(625,384);
+        av.setLocation(0,0);
+        
+        content.removeAll();
+        content.add(av, BorderLayout.CENTER);
+        content.revalidate();
+        content.repaint();
+    }//GEN-LAST:event_jMenuItem3ActionPerformed
 
     private void jMenuItem4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem4ActionPerformed
         RegistrarTipoVoluntarios rtv= new RegistrarTipoVoluntarios();
@@ -219,17 +217,6 @@ public class Voluntarios extends javax.swing.JFrame {
         content.repaint();
     }//GEN-LAST:event_jMenuItem6ActionPerformed
 
-    private void jMenuItem7ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem7ActionPerformed
-        ActualizarProfesion ap= new ActualizarProfesion();
-        ap.setSize(625,384);
-        ap.setLocation(0,0);
-        
-        content.removeAll();
-        content.add(ap, BorderLayout.CENTER);
-        content.revalidate();
-        content.repaint();
-    }//GEN-LAST:event_jMenuItem7ActionPerformed
-
     /**
      * @param args the command line arguments
      */
@@ -247,21 +234,20 @@ public class Voluntarios extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Sede.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Voluntarios2.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Sede.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Voluntarios2.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Sede.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Voluntarios2.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Sede.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Voluntarios2.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
-        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new Sede().setVisible(true);
+                new Voluntarios2().setVisible(true);
             }
         });
     }
